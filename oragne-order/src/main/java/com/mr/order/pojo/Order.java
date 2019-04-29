8159a817ec8edcfe2508eb469f15f296455db719
lzm <17635819012@163.com>
2019-04-29 15:11:30 +0800

    test 99乘法表

--- oragne-order/src/main/java/com/mr/order/pojo/Order.java
+++ oragne-order/src/main/java/com/mr/order/pojo/Order.java
@@ -6,8 +6,12 @@ package com.mr.order.pojo;
 
 public class Order {
     public static void main(String[] args) {
-        for (int i =1;i<=9;i++){
-
+        for(int k = 1;k<=9;k++){         //外循环用于控制行数
+            for(int j = 1;j<=k;j++){
+                System.out.print(j+"*"+k+"="+(j*k)+"\t");     //"\t"为制表符
+            }
+            System.out.println();  //换行
         }
     }
+
 }
