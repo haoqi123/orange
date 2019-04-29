bba30e63e1eede9617414f0e1276d3d7c146d6dc
haoqi <1148648445@qq.com>
2019-04-29 11:59:33 +0800

    asdf

---
+++ orange-loggin/src/main/java/com/mr/OrangeLogginApplication.java
@@ -0,0 +1,13 @@
+package com.mr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeLogginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangeLogginApplication.class, args);
+	}
+
+}
