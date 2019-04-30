25f1fe7f0dc6a0a4ff33039515f703b9bdeb7483
haoqi <1148648445@qq.com>
2019-04-30 14:32:18 +0800

    提交n个文件

---
+++ orange-search/src/main/java/com/mr/controller/SearchController.java
@@ -0,0 +1,31 @@
+package com.mr.controller;
+
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.common.SolrDocument;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Administer on 2019/4/30.
+ */
+@RestController
+@RequestMapping("search")
+public class SearchController {
+
+    @Autowired
+    private SolrClient solrClient;
+
+    /**
+     * 根据id查询索引
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping("getById")
+    public String getById() throws Exception {
+        SolrDocument document = solrClient.getById("1");
+        System.out.println(document);
+
+        return document.toString();
+    }
+}
---
+++ orange-search/src/main/java/com/mr/domain/SearchEntry.java
@@ -0,0 +1,12 @@
+package com.mr.domain;
+
+import org.apache.solr.client.solrj.beans.Field;
+
+/**
+ * Created by Administer on 2019/4/30.
+ */
+public class SearchEntry {
+
+    @Field("")
+    private Integer id;
+}
