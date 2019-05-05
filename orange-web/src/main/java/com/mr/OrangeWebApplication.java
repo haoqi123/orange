e24b27881f48ac2dd860fea7c230515fdb025408
spy <shang736498@163.com>
2019-05-05 10:56:25 +0800

    是是是1

--- orange-web/src/main/java/com/mr/OrangeWebApplication.java
+++ orange-web/src/main/java/com/mr/OrangeWebApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class OrangeWebApplication {
 
 	public static void main(String[] args) {
