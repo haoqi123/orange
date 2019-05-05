5035b68e8977014fce2eaffdb00579125a2bde21
lzm <17635819012@163.com>
2019-05-05 14:38:06 +0800

    class

--- orange-utils/src/main/java/com/mr/commont/commodity/Commodity.java
+++ orange-utils/src/main/java/com/mr/commont/commodity/Commodity.java
@@ -4,64 +4,82 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.omg.CORBA.PRIVATE_MEMBER;
-import org.springframework.format.annotation.DateTimeFormat;
+import org.apache.solr.client.solrj.beans.Field;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
- * 商品管理表
+ * 商品表
+ * Created by haoqi on 2019/4/30.
  */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
 public class Commodity {
-    //主键
+
+    //商品主键
+    @Field("id")
     private Integer tCommodityId;
-    //类目ID
+
+    //类目id
+    @Field("gt_id")
     private Integer gtId;
+
     //商品编号
+    @Field("c_goods_sn")
     private Integer cGoodsSn;
+
     //商品名称
+    @Field("c_name")
     private String cName;
+
     //专柜价格
+    @Field("c_counter_price")
     private Double cCounterPrice;
+
     //当前价格
+    @Field("c_retail_price")
     private Double cRetailPrice;
-    //  1 新品 2非新品 是否新品
+
+
+    //是否热卖
+    @Field("c_is_hot")
     private Integer cIsHot;
-    // 1 热品  2非热品   是否热品
+
+    //是否新品
+    @Field("c_is_new")
     private Integer cIsNew;
-    //是否在售 1在售 2非在售
+
+    //是否在售
+    @Field("c_is_on_sale")
     private Integer cIsOnSale;
+
     //关键字
+    @Field("c_keywords")
     private String cKeywords;
+
     //商品单位
+    @Field("c_unit")
     private Integer cUnit;
+
     //商品介绍
+    @Field("c_brief")
     private String cBrief;
+
     //创建时间
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Field("c_add_time")
     private Date cAddTime;
-    public String getcAddTimes(){
-        if(cAddTime != null){
-            return new SimpleDateFormat("yyyy-MM-dd").format(cAddTime);
-        }
-        return "";
-    }
+
     //修改时间
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Field("c_update_time")
     private Date cUpdateTime;
-    public String getcUpdateTimes(){
-        if(cUpdateTime != null){
-            return new SimpleDateFormat("yyyy-MM-dd").format(cUpdateTime);
-        }
-        return "";
-    }
+
     //规格
+    @Field("c_spec")
     private String cSpec;
-    //商品库存数量 默认值 0
+
+    //库存数量
+    @Field("c_number")
     private Integer cNumber;
 }
