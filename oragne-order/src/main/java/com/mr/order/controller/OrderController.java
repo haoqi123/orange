28dcd04711017d62928c7c773f51fe4d5c3b64b8
spy <shang736498@163.com>
2019-05-06 10:46:22 +0800

    一些订单实体类

--- oragne-order/src/main/java/com/mr/order/controller/OrderController.java
+++ oragne-order/src/main/java/com/mr/order/controller/OrderController.java
@@ -1,5 +1,7 @@
 package com.mr.order.controller;
 
+import com.mr.order.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,10 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class OrderController {
 
+    @Autowired
+    private OrderService orderService;
 
-    @RequestMapping("/test")
-    public String test(String name){
-        return "OK!"+name;
-    }
+
+
 
 }
