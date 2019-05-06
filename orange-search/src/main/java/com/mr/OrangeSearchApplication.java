21a339a927d873ea3f5d81881c4989f2b794d053
haoqi <1148648445@qq.com>
2019-05-06 19:40:31 +0800

    提交index 和orange-search

--- orange-search/src/main/java/com/mr/OrangeSearchApplication.java
+++ orange-search/src/main/java/com/mr/OrangeSearchApplication.java
@@ -9,9 +9,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 public class
 OrangeSearchApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(OrangeSearchApplication.class, args);
 	}
-
 }
