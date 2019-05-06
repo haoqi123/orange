4415e7b1cd125fa27122e67ae0773ca77626675f
lzm <17635819012@163.com>
2019-05-06 10:17:31 +0800

    class

--- orange-car/src/main/java/com/mr/car/controller/carController.java
+++ orange-car/src/main/java/com/mr/car/controller/carController.java
@@ -1,7 +1,13 @@
 package com.mr.car.controller;
 
+import com.mr.car.service.CarService;
+import com.mr.commont.car.Car;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 /**
  * 购物车
@@ -10,7 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("car")
 public class carController {
+    @Autowired
+    private CarService carService;
     //添加购物车
 
     //打开购物车
+    public ModelAndView queryCarList(Car car){
+        //返回指定视图
+        ModelAndView modelAndView=new ModelAndView("");
+        List list=carService.queryCarList(car);
+        modelAndView.addObject("list",list);
+        return modelAndView;
+    }
 }
---
+++ orange-car/src/main/java/com/mr/car/service/CarService.java
@@ -0,0 +1,12 @@
+package com.mr.car.service;
+
+import com.mr.commont.car.Car;
+
+import java.util.List;
+
+/**
+ * Created by DELL on 2019/5/6.
+ */
+public interface CarService {
+    List queryCarList(Car car);
+}
