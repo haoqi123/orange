384ef8e925de4b3961a32f6e437587e23d48a269
slm <18832045754@163.com>
2019-05-06 16:42:28 +0800

    zzz

---
+++ orange-loggin/src/main/java/com/mr/login/service/UserService.java
@@ -0,0 +1,11 @@
+package com.mr.login.service;
+
+import com.mr.commont.login.User;
+import com.mr.utils.ResultVo;
+
+/**
+ * Created by 师利梅 on 2019/5/6.
+ */
+public interface UserService {
+    ResultVo addUser(User user);
+}
