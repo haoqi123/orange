882cb6cda9501b1a15c6b6f99c6e04db4ed64833
lzm <17635819012@163.com>
2019-05-06 16:19:23 +0800

    class

--- orange-car/src/main/java/com/mr/car/service/impl/CarServiceImpl.java
+++ orange-car/src/main/java/com/mr/car/service/impl/CarServiceImpl.java
@@ -5,6 +5,7 @@ import com.mr.car.service.CarService;
 import com.mr.commont.car.Car;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,6 +13,7 @@ import java.util.List;
  * Created by DELL on 2019/5/6.
  */
 @Service
+@Transactional
 public class CarServiceImpl implements CarService {
     @Autowired
     private CarMapper carMapper;
