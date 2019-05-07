ea128e2a9220f751cece054a28c47a6aae7c7363
haoqi <1148648445@qq.com>
2019-05-07 20:12:30 +0800

    commodity.java

--- orange-utils/src/main/java/com/mr/commont/commodity/Commodity.java
+++ orange-utils/src/main/java/com/mr/commont/commodity/Commodity.java
@@ -22,7 +22,7 @@ public class Commodity {
 
     //商品主键
     @Field("id")
-    private String tCommodityId;
+    private String commodityId;
 
     //类目id
     @Field("gt_id")
@@ -30,64 +30,64 @@ public class Commodity {
 
     //商品编号
     @Field("c_goods_sn")
-    private Integer cGoodsSn;
+    private Integer goodsSn;
 
     //商品名称
     @Field("c_name")
-    private String cName;
+    private String name;
 
     //专柜价格
     @Field("c_counter_price")
-    private Double cCounterPrice;
+    private Double counterPrice;
 
     //当前价格
     @Field("c_retail_price")
-    private Double cRetailPrice;
+    private Double retailPrice;
 
 
     //是否热卖
     @Field("c_is_hot")
-    private Integer cIsHot;
+    private Integer isHot;
 
     //是否新品
     @Field("c_is_new")
-    private Integer cIsNew;
+    private Integer isNew;
 
     //是否在售
     @Field("c_is_on_sale")
-    private Integer cIsOnSale;
+    private Integer isOnSale;
 
     //关键字
     @Field("c_keywords")
-    private String cKeywords;
+    private String keywords;
 
     //商品单位
     @Field("c_unit")
-    private Integer cUnit;
+    private Integer unit;
 
     //商品介绍
     @Field("c_brief")
-    private String cBrief;
+    private String brief;
 
     //创建时间
     @Field("c_add_time")
-    private Date cAddTime;
+    private Date addTime;
 
     //修改时间
     @Field("c_update_time")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
-    private Date cUpdateTime;
-    public String getcUpdateTimes(){
-        if(cUpdateTime != null){
-            return new SimpleDateFormat("yyyy-MM-dd").format(cUpdateTime);
+    private Date updateTime;
+    public String getUpdateTimes(){
+        if(updateTime != null){
+            return new SimpleDateFormat("yyyy-MM-dd").format(updateTime);
         }
         return "";
     }
     //规格
     @Field("c_spec")
-    private String cSpec;
+    private String spec;
 
     //库存数量
     @Field("c_number")
-    private Integer cNumber;
+    private Integer number;
 }
